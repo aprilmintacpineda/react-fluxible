b04be31725558e3eff2dbee13488fd9cc2e96c77
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-09-16 10:49:07 +0800

    added jsdocs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@
 import { updateStore, addUpdateListener, getStore } from 'fluxible-js';
 import React from 'react';
 
+/**
+ *
+ * @param {Function} callback function that would be called sa the mutation handler. Should expect an object as the first parameter.
+ * @param  {...any} payload to the callback function
+ */
 export function dispatch (mutation, ...payload) {
   return mutation(
     {
@@ -16,6 +21,11 @@ export function dispatch (mutation, ...payload) {
   );
 }
 
+/**
+ * @param {Function} Would receive the current store's state as the only argument. Should return an object of the states that you want to be accessible in the connected component.
+ * @param {Object} You should define your action handlers here. Each methods would be called with an object (that has `updateStore` and `getStore` methods) as the first argument. The rest would be the arguments you passed to the call.
+ * @return {Object} the react component.
+ */
 export function connect (mapStatesToProps, definedMutations) {
   return WrappedComponent =>
     class Wrapper extends React.Component {
