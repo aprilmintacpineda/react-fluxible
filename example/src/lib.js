8b25c82d2e66e89e112d155e87e3984de7a30101
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-10-07 13:52:43 +0800

    used create-react-class

--- example/src/lib.js
+++ example/src/lib.js
@@ -10,27 +10,13 @@ var _fluxibleJs = require("fluxible-js");
 
 var _react = _interopRequireDefault(require("react"));
 
+var _createReactClass = _interopRequireDefault(require("create-react-class"));
+
 var _redefineStaticsJs = _interopRequireDefault(require("redefine-statics-js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 /**
  * @param {Function} callback function that would be called sa the mutation handler. Should expect an object as the first parameter.
@@ -81,41 +67,28 @@ function connect(mapStatesToProps, definedMutations) {
       })();
     }
 
-    var Wrapper =
-    /*#__PURE__*/
-    function (_React$Component) {
-      _inherits(Wrapper, _React$Component);
-
-      function Wrapper(props) {
-        var _this;
-
-        _classCallCheck(this, Wrapper);
-
-        _this = _possibleConstructorReturn(this, _getPrototypeOf(Wrapper).call(this, props));
-
-        _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "componentWillUnmount", function () {
-          // clean update listener before we unmount.
-          _this.removeListener();
-        });
-
-        _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "render", function () {
-          return _react.default.createElement(WrappedComponent, _extends({}, _this.props, mapStatesToProps ? mapStatesToProps((0, _fluxibleJs.getStore)()) : {}, mutations));
-        });
-
-        _this.state = {
+    return (0, _redefineStaticsJs.default)((0, _createReactClass.default)({
+      getInitialState: function getInitialState() {
+        return {
           count: 0
         };
-        _this.removeListener = (0, _fluxibleJs.addUpdateListener)(function () {
+      },
+      componentDidMount: function componentDidMount(props) {
+        var _this = this;
+
+        this.removeListener = (0, _fluxibleJs.addUpdateListener)(function () {
           _this.setState({
             count: _this.state.count + 1
           });
         });
-        return _this;
+      },
+      componentWillUnmount: function componentWillUnmount() {
+        // clean update listener before we unmount.
+        this.removeListener();
+      },
+      render: function render() {
+        return _react.default.createElement(WrappedComponent, _extends({}, this.props, mapStatesToProps ? mapStatesToProps((0, _fluxibleJs.getStore)()) : {}, mutations));
       }
-
-      return Wrapper;
-    }(_react.default.Component);
-
-    return (0, _redefineStaticsJs.default)(Wrapper, WrappedComponent);
+    }), WrappedComponent);
   };
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import { updateStore, addUpdateListener, getStore } from 'fluxible-js';
 import React from 'react';
+import createClass from 'create-react-class';
 import redefineStatics from 'redefine-statics-js';
 
 /**
@@ -40,34 +41,35 @@ export function connect (mapStatesToProps, definedMutations) {
       }
     }
 
-    class Wrapper extends React.Component {
-      constructor (props) {
-        super(props);
-        this.state = {
-          count: 0
-        };
-
-        this.removeListener = addUpdateListener(() => {
-          this.setState({
-            count: this.state.count + 1
+    return redefineStatics(
+      createClass({
+        getInitialState () {
+          return {
+            count: 0
+          };
+        },
+        componentDidMount (props) {
+          this.removeListener = addUpdateListener(() => {
+            this.setState({
+              count: this.state.count + 1
+            });
           });
-        });
-      }
-
-      componentWillUnmount = () => {
-        // clean update listener before we unmount.
-        this.removeListener();
-      };
-
-      render = () => (
-        <WrappedComponent
-          {...this.props}
-          {...(mapStatesToProps ? mapStatesToProps(getStore()) : {})}
-          {...mutations}
-        />
-      );
-    }
-
-    return redefineStatics(Wrapper, WrappedComponent);
+        },
+        componentWillUnmount () {
+          // clean update listener before we unmount.
+          this.removeListener();
+        },
+        render () {
+          return (
+            <WrappedComponent
+              {...this.props}
+              {...(mapStatesToProps ? mapStatesToProps(getStore()) : {})}
+              {...mutations}
+            />
+          );
+        }
+      }),
+      WrappedComponent
+    );
   };
 }
