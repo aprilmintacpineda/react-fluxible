ad4dbc0e9d0a5bfe21efa570195290dbf0ae219a
April Mintac Pineda <aprilmintacpineda@gmail.com>
2022-02-06 14:39:32 +0800

    updated deps; small code refactoring;

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,6 @@
-import { Store } from 'fluxible-js';
-export declare function createFluxibleHook<StoreType>(store: Store<StoreType>): (mapStates: (store: StoreType) => Partial<StoreType>) => Partial<StoreType>;
+import { FluxibleStore } from 'fluxible-js';
+export declare function createFluxibleHook<StoreType>(
+  store: FluxibleStore<StoreType>
+): (
+  mapStates: (store: StoreType) => Partial<StoreType>
+) => Partial<StoreType>;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Store } from 'fluxible-js';
+import { FluxibleStore } from 'fluxible-js';
+import { useEffect, useMemo, useState } from 'react';
 
-export function createFluxibleHook<StoreType>(store: Store<StoreType>) {
-  return function useFluxibleStore(mapStates: (store: StoreType) => Partial<StoreType>) {
-    const [states, setState] = React.useState(() =>
+export function createFluxibleHook<StoreType> (
+  store: FluxibleStore<StoreType>
+) {
+  return function useFluxibleStore (
+    mapStates: (store: StoreType) => Partial<StoreType>
+  ) {
+    const [states, setState] = useState(() =>
       mapStates(store.store)
     );
 
@@ -12,16 +16,24 @@ export function createFluxibleHook<StoreType>(store: Store<StoreType>) {
      * causing the observer to be registered late.
      * We want to register it as soon as this hook gets called
      */
-    const removeObserver = React.useMemo(() => {
-      return store.addObserver(() => {
-        setState(mapStates(store.store));
-      }, Object.keys(mapStates(store.store)) as Array<keyof StoreType>);
-    }, [
+    const removeObserver = useMemo(
+      () => {
+        return store.addObserver(() => {
+          setState(mapStates(store.store));
+        }, Object.keys(mapStates(store.store)) as Array<keyof StoreType>);
+      },
       // leave blank, don't watch mapStates callback
-    ]);
+      // eslint-disable-next-line
+      []
+    );
 
     // remove observer on unmount
-    React.useEffect(() => removeObserver, []);
+    useEffect(
+      () => removeObserver,
+      // leave blank, don't watch mapStates callback
+      // eslint-disable-next-line
+      []
+    );
 
     return states;
   };
